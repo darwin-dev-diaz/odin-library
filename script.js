aaadbdb737284f19f73d6fb9fe518e003ebe6905
Darwin Diaz <darwin.diaz.ut@gmail.com>
2024-02-16 11:42:22 -0700

    Complete createNewBook function

--- script.js
+++ script.js
@@ -21,12 +21,58 @@ function isFormFilled(form) {
   }
   return true;
 }
+
+function createNewBook(title, author, pageNum, isRead) {
+  // returns a book object
+  const bookClassList = [
+    "book-one",
+    "book-two",
+    "book-three",
+    "book-four",
+    "book-five",
+    "book-six",
+    "book-seven",
+  ];
+  const book = document.createElement("div");
+  book.classList.add("book", bookClassList[Math.floor(Math.random() * 7)]);
+
+  const bookTitle = document.createElement("div");
+  bookTitle.classList.add("book-title");
+  bookTitle.textContent = title;
+  book.appendChild(bookTitle);
+
+  const bookAuthor = document.createElement("div");
+  bookAuthor.classList.add("book-author");
+  bookAuthor.textContent = author;
+  book.appendChild(bookAuthor);
+
+  const cardPopUp = document.createElement("div");
+  cardPopUp.classList.add("cardPopUp");
+
+  const pages = document.createElement("div");
+  pages.classList.add("pages");
+  pages.textContent = `PAGES: ${pageNum}`;
+  cardPopUp.appendChild(pages);
+  
+  const readButton = document.createElement("button");
+  const readStatus = isRead ? 'read' : 'not-read';
+  readButton.classList.add("read-btn", readStatus);
+  cardPopUp.appendChild(readButton);
+  
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete-btn");
+  deleteButton.textContent = `DELETE`;
+  cardPopUp.appendChild(deleteButton);
+
+  book.appendChild(cardPopUp);
+
+  return book;
+}
 const submitButton = document.querySelector(".submit-button");
 submitButton.addEventListener("click", (event) => {
   const formL = document.querySelector("form");
-  // event.stopPropagation();
   if (isFormFilled(form)) {
-      event.preventDefault();
-      toggleAddBook();
+    event.preventDefault();
+    toggleAddBook();
   }
 });
