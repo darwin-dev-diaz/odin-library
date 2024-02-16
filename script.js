4c54cc4cd7ac370a4fb1a8ecdc4df2cd854aeb5d
Darwin Diaz <darwin.diaz.ut@gmail.com>
2024-02-16 11:19:39 -0700

    Wire pop up

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 const addBookBtn = document.querySelector(".add-book-btn");
-addBookBtn.addEventListener('click',()=>{
-    if(addBookBtn.classList.toggle("clicked"));
+const shelf = document.querySelector(".shelf");
+const addBookPopUp = document.querySelector(".add-book-popup");
+function toggleAddBook() {
+  addBookBtn.classList.toggle("clicked");
+  shelf.classList.toggle("no-display");
+  addBookPopUp.classList.toggle("no-display");
+}
+// toggle add book screen
+addBookBtn.addEventListener("click", () => {
+  toggleAddBook();
+});
+
+//wire add book pop up
+function isFormFilled(form) {
+  const children = form.children;
+  for (child of children) {
+    if (child.hasAttribute("required") && child.value === "") {
+      return false;
+    }
+  }
+  return true;
+}
+const submitButton = document.querySelector(".submit-button");
+submitButton.addEventListener("click", (event) => {
+  const formL = document.querySelector("form");
+  // event.stopPropagation();
+  if (isFormFilled(form)) {
+      event.preventDefault();
+      toggleAddBook();
+  }
 });
