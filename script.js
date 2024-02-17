c257c2ae79b9ee9f9e87632e9d3a91703735cb67
Darwin Diaz <darwin.diaz.ut@gmail.com>
2024-02-17 08:33:54 -0700

    Create library arr and book construcgtor

--- script.js
+++ script.js
@@ -22,7 +22,17 @@ function isFormFilled(form) {
   return true;
 }
 
-function createNewBook(title, author, pageNum, isRead) {
+
+function Book(title,author,pageNum, isRead){
+  this.title = title;
+  this.author = author;
+  this.pageNum = pageNum;
+  this.isRead = isRead;
+}
+
+const library = [new Book('Title','author',23,false), new Book('Title2','author2',232,true)];
+
+function createNewBook(bookObj) {
   // returns a book object
   const bookClassList = [
     "book-one",
@@ -33,29 +43,29 @@ function createNewBook(title, author, pageNum, isRead) {
     "book-six",
     "book-seven",
   ];
-  const book = document.createElement("div");
-  book.classList.add("book", bookClassList[Math.floor(Math.random() * 7)]);
+  const bookElement = document.createElement("div");
+  bookElement.classList.add("book", bookClassList[Math.floor(Math.random() * 7)]);
 
   const bookTitle = document.createElement("div");
   bookTitle.classList.add("book-title");
-  bookTitle.textContent = title;
-  book.appendChild(bookTitle);
+  bookTitle.textContent = bookObj.title;
+  bookElement.appendChild(bookTitle);
 
   const bookAuthor = document.createElement("div");
   bookAuthor.classList.add("book-author");
-  bookAuthor.textContent = author;
-  book.appendChild(bookAuthor);
+  bookAuthor.textContent = bookObj.author;
+  bookElement.appendChild(bookAuthor);
 
   const cardPopUp = document.createElement("div");
   cardPopUp.classList.add("cardPopUp");
 
   const pages = document.createElement("div");
   pages.classList.add("pages");
-  pages.textContent = `PAGES: ${pageNum}`;
+  pages.textContent = `PAGES: ${bookObj.pageNum}`;
   cardPopUp.appendChild(pages);
   
   const readButton = document.createElement("button");
-  const readStatus = isRead ? 'read' : 'not-read';
+  const readStatus = bookObj.isRead ? 'read' : 'not-read';
   readButton.classList.add("read-btn", readStatus);
   cardPopUp.appendChild(readButton);
   
@@ -64,15 +74,18 @@ function createNewBook(title, author, pageNum, isRead) {
   deleteButton.textContent = `DELETE`;
   cardPopUp.appendChild(deleteButton);
 
-  book.appendChild(cardPopUp);
+  bookElement.appendChild(cardPopUp);
 
-  return book;
+  return bookElement;
 }
+
+
+
 const submitButton = document.querySelector(".submit-button");
 submitButton.addEventListener("click", (event) => {
   const formL = document.querySelector("form");
   if (isFormFilled(form)) {
-    event.preventDefault();
     toggleAddBook();
+    event.preventDefault();
   }
 });
